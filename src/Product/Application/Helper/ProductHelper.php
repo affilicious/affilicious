f02b8fe3a8bcbfb10d159c68d57a7d8a1abb0e8d
Alexander Barton <alexander.barton@outlook.com>
2016-10-24 18:27:10 +0200

    Removed the shop helper method

--- src/Product/Application/Helper/ProductHelper.php
+++ src/Product/Application/Helper/ProductHelper.php
@@ -3,9 +3,6 @@ namespace Affilicious\Product\Application\Helper;
 
 use Affilicious\Product\Domain\Model\Product;
 use Affilicious\Product\Domain\Model\ProductId;
-use Affilicious\Product\Domain\Model\Shop\Shop;
-use Affilicious\Product\Domain\Model\Shop\ShopId;
-use Affilicious\Product\Domain\Model\Variant\ProductVariant;
 
 if(!defined('ABSPATH')) {
     exit('Not allowed to access pages directly.');
@@ -53,44 +50,5 @@ class ProductHelper
         }
 
         return $product;
-    }
-
-    /**
-     * Get the active shop of the product
-     * If you pass in nothing as a product, the current post will be used.
-     * If you pass in nothing as a shop, the first shop will be used.
-     *
-     * @since 0.3
-     * @param int|\WP_Post|Product|null $productOrId
-     * @param int|\WP_Post|Shop|null $shopOrId
-     * @return null|Shop
-     */
-    public static function getShop($productOrId = null, $shopOrId = null)
-    {
-        $product = self::getProduct($productOrId);
-        if($product === null) {
-            return null;
-        }
-
-        if ($shopOrId === null) {
-            $shops = $product->getShops();
-	        $shop = !empty($shops) ? $shops[0] : null;
-
-            return $shop;
-        }
-
-        if (is_int($shopOrId)) {
-            $shopId = $shopOrId;
-        } elseif (is_array($shopOrId) && !empty($shopOrId['shop_id'])) {
-            $shopId = $shopOrId['shop_id'];
-        } elseif ($shopOrId instanceof \WP_Post) {
-            $shopId = $shopOrId->ID;
-        } else {
-            $shopId = $shopOrId->getId()->getValue();
-        }
-
-        $shop = $product->getShop(new ShopId($shopId));
-
-        return $shop;
     }
 }
