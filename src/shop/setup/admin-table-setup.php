630af3bf81f9ae166f70a410b808c68053198e7e
Alexander Barton <alexander.barton@outlook.com>
2017-01-22 16:11:36 +0100

    Fixed the shop admin table

--- src/shop/setup/admin-table-setup.php
+++ src/shop/setup/admin-table-setup.php
@@ -71,7 +71,7 @@ class Admin_Table_Setup
 
         if ($column_name == 'provider') {
             $provider_id = carbon_get_term_meta($term_id, Carbon_Shop_Template_Repository::PROVIDER);
-            if(!empty($provider_id)) {
+            if(!empty($provider_id) && $provider_id != 'none') {
                 $provider = $this->provider_repository->find_by_id(new Provider_Id($provider_id));
                 $value = $provider === null ?: $provider->get_name()->get_value();
             }
