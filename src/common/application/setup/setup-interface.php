adec9ce82e544c2c1982b64071e23571adee3553
Alexander Barton <alexander.barton@outlook.com>
2017-01-29 22:16:36 +0100

    Added the setup interface for legacy purpose

---
+++ src/common/application/setup/setup-interface.php
@@ -0,0 +1,13 @@
+<?php
+namespace Affilicious\Common\Filter;
+
+if (!defined('ABSPATH')) {
+    exit('Not allowed to access pages directly.');
+}
+
+/**
+ * @deprecated
+ */
+interface Setup_Interface
+{
+}
