82d09879d68555a8750ff79bcb1931e65ba8d400
Alexander Barton <alexander.barton@outlook.com>
2017-02-09 11:52:16 +0100

    Filtered the product relations a little bit more

--- src/product/setup/product-setup.php
+++ src/product/setup/product-setup.php
@@ -578,7 +578,7 @@ class Product_Setup
                 $current_post = get_post();
 
                 foreach ($options as $key => $option) {
-                    if($option['id'] == $current_post->ID) {
+                    if($option['id'] == $current_post->ID || $option['post_status'] == 'publish' || $option['post_type'] != Product::POST_TYPE) {
                         unset($options[$key]);
                     }
                 }
