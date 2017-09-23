5c3b39a4ccb645bd88889a29926953f5e216a0c4
Alexander Barton <alexander.barton@outlook.com>
2017-09-23 11:49:12 +0200

    Int values will be converted to string in the affiliate product ID

--- src/shop/model/affiliate-product-id.php
+++ src/shop/model/affiliate-product-id.php
@@ -20,7 +20,11 @@ class Affiliate_Product_Id
      */
     public function __construct($value)
     {
-    	Assert_Helper::is_string_not_empty($value, __METHOD__, 'The affiliate ID must be a non empty string. Got: %s', '0.9.2');
+    	if(is_numeric($value)) {
+    		$value = strval($value);
+	    }
+
+    	Assert_Helper::is_string_not_empty($value, __METHOD__, 'The affiliate product ID must be a non empty string. Got: %s', '0.9.2');
 
         $this->set_value($value);
     }
