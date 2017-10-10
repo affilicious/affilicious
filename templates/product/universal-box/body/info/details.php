f5ba97591be790a76d66fe31f218a658ed49d874
Alexander Barton <alexander.barton@outlook.com>
2017-10-10 19:50:59 +0200

    Optimized boolean detail values

--- templates/product/universal-box/body/info/details.php
+++ templates/product/universal-box/body/info/details.php
@@ -15,7 +15,17 @@ $product = !empty($product) ? $product : aff_get_product();
                     <?php if($detail['type'] == 'file'): ?>
                         <td class="aff-product-details-item-value-file aff-product-details-item-value"><?php echo wp_get_attachment_link($detail['value'], 'medium', false, false, __('Download', 'affilicious-shop-theme')); ?></td>
                     <?php elseif($detail['type'] == 'boolean'): ?>
-                        <td class="aff-product-details-item-value-boolean aff-product-details-item-value"><?php if($detail['value']): ?><i class="aff-product-details-item-value-boolean-check"></i><?php else: ?><i class="fa fa-close text-danger"></i><?php endif; ?></td>
+                        <td class="aff-product-details-item-value-boolean aff-product-details-item-value">
+                            <?php if($detail['value'] == 'yes'): ?>
+                                <svg class="aff-product-details-item-value-boolean-check" viewBox="0 0 1792 1792" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1671 566q0 40-28 68l-724 724-136 136q-28 28-68 28t-68-28l-136-136-362-362q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 295 656-657q28-28 68-28t68 28l136 136q28 28 28 68z" transform="matrix(1, 0, 0, 1, 0, 2.842170943040401e-14)"/>
+                                </svg>
+                            <?php else: ?>
+                                <svg class="aff-product-details-item-value-boolean-cross" viewBox="0 0 1792 1792" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z" transform="matrix(1, 0, 0, 1, 0, 2.842170943040401e-14)"/>
+                                </svg>
+                            <?php endif; ?>
+                        </td>
                     <?php else: ?>
                         <td class="aff-product-details-item-value-text aff-product-details-item-value"><?php echo esc_html($detail['value']); ?> <?php echo esc_html($detail['unit']); ?></td>
                     <?php endif; ?>
