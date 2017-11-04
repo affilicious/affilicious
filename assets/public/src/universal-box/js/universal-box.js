5c14c6e5c2a1df72a627bc6ee600df0f4c2137e4
Alexander Barton <alexander.barton@outlook.com>
2017-11-04 20:00:48 +0100

    Added universal box image gallery controls

--- assets/public/src/universal-box/js/universal-box.js
+++ assets/public/src/universal-box/js/universal-box.js
@@ -1,13 +1,10 @@
 jQuery(function($) {
-    $(document).ready(function() {
-        $(".aff-product-image-gallery").lightSlider({
-            item: 1,
-            speed: 1000, // ms
-            pause: 8000, // ms
-            auto: true,
-            loop: true,
-            pager: false,
-            controls: false,
-        });
+    $(".aff-product-image-gallery").lightSlider({
+        item: 1,
+        speed: 1000, // ms
+        pause: 8000, // ms
+        auto: true,
+        loop: true,
+        pager: false,
     });
 });
