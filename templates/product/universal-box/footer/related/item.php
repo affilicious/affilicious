5d10ec6fbff53ada7042cd52b6ceaf7df37b795b
Alexander Barton <alexander.barton@outlook.com>
2017-11-04 19:45:05 +0100

    Added product link for related items without a image

--- templates/product/universal-box/footer/related/item.php
+++ templates/product/universal-box/footer/related/item.php
@@ -6,15 +6,15 @@ $product = !empty($product) ? $product : aff_get_product();
 <?php do_action('affilicious_template_before_product_universal_box_related_item', $product); ?>
 
 <article class="aff-product-related-item">
-	<?php if(has_post_thumbnail()): ?>
-        <a href="<?php the_permalink() ?>">
+    <a href="<?php the_permalink() ?>">
+        <?php if(has_post_thumbnail()): ?>
             <?php the_post_thumbnail('post-thumbnail', [
                 'class' => 'aff-product-related-item-thumbnail',
             ]); ?>
-        </a>
-	<?php else: ?>
-        <img class="aff-product-related-item-thumbnail" alt="<?php the_title(); ?>" src="<?php echo AFFILICIOUS_ROOT_URL . 'assets/public/dist/img/no-image.png'?>">
-	<?php endif; ?>
+        <?php else: ?>
+            <img class="aff-product-related-item-thumbnail" alt="<?php the_title(); ?>" src="<?php echo AFFILICIOUS_ROOT_URL . 'assets/public/dist/img/no-image.png'?>">
+        <?php endif; ?>
+    </a>
 </article>
 
 <?php do_action('affilicious_template_after_product_universal_box_related_item', $product); ?>
