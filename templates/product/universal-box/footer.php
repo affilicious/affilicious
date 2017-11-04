b4f86a4e0843c8429918b5cede3a12ee3b0f1360
Kristian Strehlau <krischi.s@web.de>
2017-11-04 14:47:18 +0100

    Added some fixes for related products in the universal box

--- templates/product/universal-box/footer.php
+++ templates/product/universal-box/footer.php
@@ -6,7 +6,7 @@ $product = !empty($product) ? $product : aff_get_product();
 <?php do_action('affilicious_template_before_product_universal_box_footer', $product); ?>
 
 <footer class="aff-product-universal-box-footer aff-product-universal-box-row">
-	<?php if(aff_has_product_related_products() && aff_has_product_related_accessories()): ?>
+	<?php if(aff_has_product_related_products() || aff_has_product_related_accessories()): ?>
 	    <?php aff_render_template('product/universal-box/footer/related', ['product' => $product]); ?>
     <?php endif; ?>
 </footer>
