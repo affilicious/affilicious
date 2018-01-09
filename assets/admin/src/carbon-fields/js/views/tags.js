f382bc6c6817ab84f8f279ec272a63ca14db2eff
Alexander Barton <alexander.barton@outlook.com>
2018-01-09 13:14:13 +0200

    Fixed and optimized the tags field JS for Carbon Fields

--- assets/admin/src/carbon-fields/js/views/tags.js
+++ assets/admin/src/carbon-fields/js/views/tags.js
@@ -1,21 +1,57 @@
-export default carbon.fields.View.extend({
-    initialize: function() {
+/**
+ * The view of the tags field used for Carbon Fields.
+ *
+ * @since 0.8.16
+ */
+let Tags = carbon.fields.View.extend({
+
+    /**
+     * Initialize the tags view.
+     *
+     * @since 0.8.16
+     * @public
+     */
+    initialize() {
         carbon.fields.View.prototype.initialize.apply(this);
 
-        this.on('field:rendered', this.initField);
+        this.on('field:rendered', this._initTags);
+        this.listenTo(this.model, 'change:visible', this._initTags);
     },
 
-    initField: function() {
-        let self = this,
-            tags = this.model.get('tags'),
-            maxItems = this.model.get('maxItems');
+    /**
+     * Initialize the tags with Selectize.
+     *
+     * @since 0.9.18
+     * @private
+     */
+    _initTags() {
+        let tags = this.model.get('tags');
+        let maxItems = this.model.get('maxItems');
 
         if(tags) {
-            let options = Object.keys(tags).map((key) => {
-                return {id: key, text: tags[key]}
-            });
+            this._initPredefinedTags(tags, maxItems);
+        } else {
+            this._initCustomTags(maxItems);
+        }
+    },
 
-            this.$el.find('.aff-tags.aff-tags-predefined').selectize({
+    /**
+     * Initialize the predefined tags with auto-completion.
+     *
+     * @since 0.9.18
+     * @private
+     * @param {array} tags
+     * @param {int} maxItems
+     */
+    _initPredefinedTags(tags, maxItems) {
+        let select = this.$el.find('.aff-tags-predefined');
+        let selectize = select[0].selectize;
+        let options = Object.keys(tags).map((key) => {
+            return {id: key, text: tags[key]}
+        });
+
+        if(!selectize) {
+            select.selectize({
                 plugins: ['drag_drop', 'remove_button'],
                 delimiter: ',',
                 persist: true,
@@ -26,21 +62,48 @@ export default carbon.fields.View.extend({
                 searchField: ['text'],
                 maxItems: maxItems,
                 options: options,
-                onChange() {
-                    self.model.set('value', self.$el.find('input[name="' + self.templateVariables.name +'"]').val());
-                },
+                onChange: () => this._onChangeTags(),
             });
         } else {
-            this.$el.find('.aff-tags.aff-tags-custom').selectize({
+            selectize.enable();
+        }
+    },
+
+    /**
+     * Initialize the custom tags without auto-completion.
+     *
+     * @private
+     * @since 0.9.18
+     * @param {int} maxItems
+     */
+    _initCustomTags(maxItems) {
+        let select = this.$el.find('.aff-tags-custom');
+        let selectize = select[0].selectize;
+
+        if(!selectize) {
+            select.selectize({
                 plugins: ['drag_drop', 'remove_button'],
                 delimiter: ',',
                 persist: true,
                 maxItems: maxItems,
                 create: true,
-                onChange() {
-                    self.model.set('value', self.$el.find('input[name="' + self.templateVariables.name +'"]').val());
-                },
+                onChange: () => this._onChangeTags(),
             });
+        } else {
+            selectize.enable();
         }
     },
+
+    /**
+     * Set the new value of the model if the tags have changed.
+     *
+     * @since 0.9.18
+     * @private
+     */
+    _onChangeTags() {
+        let input = this.$el.find('input[name="' + this.templateVariables.name + '"]');
+        this.model.set('value', input.val());
+    }
 });
+
+export default Tags;
